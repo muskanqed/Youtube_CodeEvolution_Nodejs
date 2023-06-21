0ed43a11ad9fe57c272dc14485df91ccf6de2700
muskanqed <muskanqed@gmail.com>
2023-06-22 00:43:31 +0530

    Hello World program using Nodejs

---
+++ index.js
@@ -0,0 +1,10 @@
+// Executing Javascript with Node REPL(Read,Evaluate,Print,Loop)
+// Type node to start an interactive shell and type the code
+
+// Executing Javascript with .js file and run the node command in the terminal
+// To run the below code write node index.js in the terminal
+
+
+
+console.log("Hello World from index.js");
+
