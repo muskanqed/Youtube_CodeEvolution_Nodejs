3d57df3922b95a8c5a66dbbfad0b5c706809719f
muskanqed <muskanqed@gmail.com>
2023-06-22 00:55:34 +0530

    Types of Modules in Nodejs

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
 // Executing Javascript with .js file and run the node command in the terminal
 // To run the below code write node index.js in the terminal
 
-
+const add = require("./add");
 
 console.log("Hello World from index.js");
-
+const sum = add(1, 2);
+const sum2 = add(9, 9);
+console.log(sum);
+console.log(sum2);
